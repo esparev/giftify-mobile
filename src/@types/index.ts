fb327f264dd2f78532ef230962c9ffc979a5bbb0
JoseMa <esparev@hotmail.com>
2023-05-18 11:41:51 -0600

    types: 🛡️ add address prop types and re-organize existent types

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,5 +1,33 @@
 import { RouteProp } from '@react-navigation/native';
 
+export interface AuthContextProps {
+  user: UserProps | undefined;
+  token: string | undefined;
+}
+export interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export interface AddressProps {
+  id: string | undefined;
+  streetName: string;
+  streetNumber: string;
+  postalCode: string;
+  city: string;
+  area: string;
+  locality: string | undefined;
+  country: string | undefined;
+  createdAt: string | undefined;
+}
+export interface GiftProps {
+  id: string | undefined;
+  name: string;
+  image: string;
+  rating: number;
+  price: number;
+  description: string | undefined;
+  createdAt: string | undefined;
+}
 export interface UserProps {
   id: string;
   username: string;
@@ -10,42 +38,29 @@ export interface UserProps {
   avatar: string;
 }
 
-export interface AuthContextProps {
-  user: UserProps | undefined;
-  token: string | undefined;
+export interface AddressListProps {
+  addresses: AddressProps[];
 }
-
-export interface AuthProviderProps {
-  children: React.ReactNode;
-}
-
-export interface GiftProps {
-  id: string | undefined;
-  name: string;
-  image: string;
-  rating: number;
-  price: number;
-  description: string | undefined;
-  createdAt: string | undefined;
-}
-
 export interface GiftListProps {
   gifts: GiftProps[];
 }
 
 type RootStackParamList = {
   Gift: { id: string };
-  Profile: {username: string};
-  Settings: {username: string};
+  Profile: { username: string };
+  Settings: { username: string };
+  Addresses: { userId: string };
 };
-
+type AddressRouteProp = RouteProp<RootStackParamList, 'Addresses'>;
 type GiftRouteProp = RouteProp<RootStackParamList, 'Gift'>;
 type ProfileRouteProp = RouteProp<RootStackParamList, 'Profile'>;
 type SettingsRouteProp = RouteProp<RootStackParamList, 'Settings'>;
-
 export interface GiftScreenProps {
   route: GiftRouteProp;
 }
 export interface ProfileScreenProps {
   route: ProfileRouteProp | SettingsRouteProp;
+}
+export interface UserIdProps {
+  route: AddressRouteProp;
 }
