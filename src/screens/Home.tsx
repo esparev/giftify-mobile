2ef0be04d86240731920d52397f41e570e7b1daa
JoseMa <esparev@hotmail.com>
2023-05-23 08:41:11 -0600

    feat: ✨ fetch home data from api rest

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity, View, Image, ActivityIndicator } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { useNavigation } from '@react-navigation/native';
-import useHome from '../graphql/useHome';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import { UserProps } from '../@types';
+import getHome from '../api/getHome';
 import { getData } from '../storage';
 import { TextRegular, TextSemiBold } from '../components/CustomText';
 import GiftList from '../components/Lists/GiftList';
@@ -14,17 +15,44 @@ const cart = '../assets/icons/cart.png';
 const profile = '../assets/static/default-gift.png';
 const promotion = '../assets/static/promotion.png';
 
-let userUsername: string;
-
-(async () => {
-  const userData = await getData();
-  userUsername = userData.user.username;
-})();
-
 const Home = (): JSX.Element => {
-  const { loading, error, data } = useHome(userUsername);
+  const isFocused = useIsFocused();
+  const [user, setUser] = useState<UserProps>({
+    id: '',
+    username: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    role: '',
+    avatar: '',
+  });
+  const [gifts, setGifts] = useState([]);
+  const [loading, setLoading] = useState(true);
   const navigation = useNavigation<StackNavigationProp<any>>();
-  const avatar = data?.user?.avatar;
+
+  const loadData = async () => {
+    try {
+      let userUsername: string;
+      let token: string;
+
+      const userData = await getData();
+      userUsername = userData.user.username;
+      token = userData.token;
+
+      const { user, gifts } = await getHome(userUsername, token);
+      setUser(user);
+      setGifts(gifts);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      await loadData();
+    })();
+  }, [isFocused]);
 
   return (
     <View style={home.main}>
@@ -43,15 +71,15 @@ const Home = (): JSX.Element => {
         {/* Cart & Profile */}
         <View style={home.cartProfile}>
           <TouchableOpacity
-            onPress={() => navigation.push('Cart', { userId: data?.user?.id })}>
+            onPress={() => navigation.push('Cart', { userId: user.id })}>
             <Image source={require(cart)} style={icon.size} />
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() =>
-              navigation.push('Profile', { username: userUsername })
+              navigation.push('Profile', { username: user.username })
             }>
             <Image
-              source={loading ? require(profile) : { uri: avatar }}
+              source={loading ? require(profile) : { uri: user.avatar }}
               style={icon.profile}
             />
           </TouchableOpacity>
@@ -67,7 +95,7 @@ const Home = (): JSX.Element => {
       {loading ? (
         <ActivityIndicator color="#ffffff" />
       ) : (
-        <GiftList gifts={data?.gifts} />
+        <GiftList gifts={gifts} />
       )}
     </View>
   );
